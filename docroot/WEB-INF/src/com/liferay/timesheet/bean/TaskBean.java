c0dc29dba18081f11d45ada2285bae92c1b36f1a
Zsolt Szabo <zsolt.szabo@liferay.com>
2013-11-29 16:03:58 +0100

    issue#11 Create TaskSessionBean managed bean

--- docroot/WEB-INF/src/com/liferay/timesheet/bean/TaskBean.java
+++ docroot/WEB-INF/src/com/liferay/timesheet/bean/TaskBean.java
@@ -15,6 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
@@ -31,70 +32,22 @@ public class TaskBean implements Serializable{
 
 	private static final long serialVersionUID = -8412810082872360906L;
 
-	private TaskSession currentTaskSession;
-	private Date endTime;
-	private Date startTime;
 	private String taskName;
 
-	public String createTaskSession()
-		throws ParseException, PortalException, SystemException {
+	@ManagedProperty(name = "taskSessionBean", value = "#{taskSessionBean}")
+	private TaskSessionBean taskSessionBean;
+
+	public String createTask()
+		throws PortalException, SystemException, ParseException {
 
 		long userId = TimesheetUtil.getCurrentUserId();
 
 		Task task = TaskLocalServiceUtil.addTask(taskName, userId);
 
-		long taskId = task.getTaskId();
-
-		Date startDate = new Date();
-		Date todayWithoutTime = null;
-
-		if (startTime != null) {
-			todayWithoutTime = TimesheetUtil.getTodayWithoutTime();
-
-			startDate =
-				TimesheetUtil.addDateToDate(todayWithoutTime, getStartTime());
-		}
-
-		closeCurrentTaskSession(userId, startDate);
-
-		TaskSessionLocalServiceUtil.addTaskSession(startDate, taskId, userId);
+		taskSessionBean.setSelectedTask(task);
+		taskSessionBean.createTaskSession();
 
 		return "success";
-	}
-
-	public String finishTaskSession() throws ParseException, SystemException {
-		long userId = TimesheetUtil.getCurrentUserId();
-
-		TaskSession currentTaskSession =
-			TaskSessionLocalServiceUtil.getCurrentTaskSession(userId);
-
-		Date endDate = new Date();
-		Date todayWithoutTime = null;
-
-		if (endTime != null) {
-			todayWithoutTime = TimesheetUtil.getTodayWithoutTime();
-
-			endDate =
-				TimesheetUtil.addDateToDate(todayWithoutTime, getEndTime());
-		}
-
-		currentTaskSession.setEndTime(endDate);
-
-		TaskSessionLocalServiceUtil.updateTaskSession(currentTaskSession);
-
-		return "success";
-	}
-
-	public TaskSession getCurrentTaskSession() {
-		return currentTaskSession;
-	}
-
-	public Date getEndTime() {
-		return endTime;
-	}
-
-	public Date getStartTime() {
-		return startTime;
 	}
 
 	public List<Task> getTaskByUser() throws PortalException, SystemException {
@@ -109,59 +62,16 @@ public class TaskBean implements Serializable{
 		return taskName;
 	}
 
-	/**
-	 *  Currently it gives back TaskSessions only for the current day.
-	 *  TO DO: Passing date parameter from xhtml, so that it will be more
-	 *  generic.
-	 * @return
-	 * @throws ParseException 
-	 * @throws SystemException 
-	 */
-	public List<TaskSession> getTaskSessionsByD_U()
-		throws ParseException, SystemException {
-
-		long userId = TimesheetUtil.getCurrentUserId();
-
-		List<TaskSession> taskSessions =
-			TaskSessionLocalServiceUtil.getTaskSessionsByD_U(
-				TimesheetUtil.getTodayWithoutTime(), userId);
-
-		if (taskSessions == null) {
-			taskSessions = Collections.emptyList();
-		}
-
-		return taskSessions;
-	}
-
-	public void setCurrentTaskSession(TaskSession currentTaskSession) {
-		this.currentTaskSession = currentTaskSession;
-	}
-
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
-	}
-
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
+	public TaskSessionBean getTaskSessionBean() {
+		return taskSessionBean;
 	}
 
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
 
-	private TaskSession closeCurrentTaskSession(long userId, Date endDate)
-		throws SystemException {
-
-		TaskSession currentTaskSession =
-			TaskSessionLocalServiceUtil.getCurrentTaskSession(userId);
-
-		if (currentTaskSession != null) {
-			currentTaskSession.setEndTime(endDate);
-
-			TaskSessionLocalServiceUtil.updateTaskSession(currentTaskSession);
-		}
-
-		return currentTaskSession;
+	public void setTaskSessionBean(TaskSessionBean taskSessionBean) {
+		this.taskSessionBean = taskSessionBean;
 	}
 
 }
---
+++ docroot/WEB-INF/src/com/liferay/timesheet/bean/TaskSessionBean.java
@@ -0,0 +1,153 @@
+package com.liferay.timesheet.bean;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.timesheet.NoSelectedTaskException;
+import com.liferay.timesheet.model.Task;
+import com.liferay.timesheet.model.TaskSession;
+import com.liferay.timesheet.service.TaskSessionLocalServiceUtil;
+import com.liferay.timesheet.util.TimesheetUtil;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class TaskSessionBean implements Serializable {
+
+	private static final long serialVersionUID = -7318596896784233150L;
+
+	private TaskSession currentTaskSession;
+	private Date endTime;
+	private Date startTime;
+	private Task selectedTask;
+
+	public String createTaskSession()
+		throws ParseException, PortalException, SystemException {
+
+		if (selectedTask == null) {
+			throw new NoSelectedTaskException();
+		}
+
+		long taskId = selectedTask.getTaskId();
+
+		Date startDate = new Date();
+		Date todayWithoutTime = null;
+
+		if (startTime != null) {
+			todayWithoutTime = TimesheetUtil.getTodayWithoutTime();
+
+			startDate =
+				TimesheetUtil.addDateToDate(todayWithoutTime, getStartTime());
+		}
+
+		long userId = TimesheetUtil.getCurrentUserId();
+
+		closeCurrentTaskSession(userId, startDate);
+
+		TaskSessionLocalServiceUtil.addTaskSession(startDate, taskId, userId);
+
+		return "success";
+	}
+
+	private TaskSession closeCurrentTaskSession(long userId, Date endDate)
+		throws SystemException {
+
+		TaskSession currentTaskSession =
+			TaskSessionLocalServiceUtil.getCurrentTaskSession(userId);
+
+		if (currentTaskSession != null) {
+			currentTaskSession.setEndTime(endDate);
+
+			TaskSessionLocalServiceUtil.updateTaskSession(currentTaskSession);
+		}
+
+		return currentTaskSession;
+	}
+
+	public String finishTaskSession() throws ParseException, SystemException {
+		long userId = TimesheetUtil.getCurrentUserId();
+
+		TaskSession currentTaskSession =
+			TaskSessionLocalServiceUtil.getCurrentTaskSession(userId);
+
+		Date endDate = new Date();
+		Date todayWithoutTime = null;
+
+		if (endTime != null) {
+			todayWithoutTime = TimesheetUtil.getTodayWithoutTime();
+
+			endDate =
+				TimesheetUtil.addDateToDate(todayWithoutTime, getEndTime());
+		}
+
+		currentTaskSession.setEndTime(endDate);
+
+		TaskSessionLocalServiceUtil.updateTaskSession(currentTaskSession);
+
+		return "success";
+	}
+
+	public TaskSession getCurrentTaskSession() {
+		return currentTaskSession;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public Task getSelectedTask() {
+		return selectedTask;
+	}
+
+	/**
+	 *  Currently it gives back TaskSessions only for the current day.
+	 *  TO DO: Passing date parameter from xhtml, so that it will be more
+	 *  generic.
+	 * @return
+	 * @throws ParseException 
+	 * @throws SystemException 
+	 */
+	public List<TaskSession> getTaskSessionsByD_U()
+		throws ParseException, SystemException {
+
+		long userId = TimesheetUtil.getCurrentUserId();
+
+		List<TaskSession> taskSessions =
+			TaskSessionLocalServiceUtil.getTaskSessionsByD_U(
+				TimesheetUtil.getTodayWithoutTime(), userId);
+
+		if (taskSessions == null) {
+			taskSessions = Collections.emptyList();
+		}
+
+		return taskSessions;
+	}
+
+	public void setCurrentTaskSession(TaskSession currentTaskSession) {
+		this.currentTaskSession = currentTaskSession;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public void setSelectedTask(Task selectedTask) {
+		this.selectedTask = selectedTask;
+	}
+
+}
