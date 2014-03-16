2e812dfb191228e80fc6118e5ca8935e3f5148dc
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-03-16 07:14:59 +0100

    issue#39 Added new finder method C_I_U

--- docroot/WEB-INF/src/com/liferay/timesheet/service/persistence/TaskSessionFinderImpl.java
+++ docroot/WEB-INF/src/com/liferay/timesheet/service/persistence/TaskSessionFinderImpl.java
@@ -17,29 +17,38 @@ import java.util.List;
 public class TaskSessionFinderImpl extends BasePersistenceImpl<TaskSession>
 	implements TaskSessionFinder {
 
-	public static String FIND_BY_DATE_USERID =
-		TaskSessionFinder.class.getName() + ".findByD_U";
+	public static String FIND_BY_COMPANYID_INTERVAL_USERID =
+		TaskSessionFinder.class.getName() + ".findByC_I_U";
 
-	public List<TaskSession> findByD_U(
-		Date date, long userId, int start, int end) {
+	public List<TaskSession> findByC_I_U(
+		long companyId, Date date1, Date date2, long userId) {
 
 		Session session = null;
 
 		session = openSession();
 
-		String sql = CustomSQLUtil.get(FIND_BY_DATE_USERID);
+		String sql = CustomSQLUtil.get(FIND_BY_COMPANYID_INTERVAL_USERID);
 
 		SQLQuery q = session.createSQLQuery(sql);
 
-		q.addScalar("startTime", Type.DATE);
-		q.addScalar("endTime", Type.DATE);
-		q.addScalar("taskName", Type.STRING);
+		q.addScalar("taskSessionId", Type.LONG);
+		q.addScalar("companyId", Type.LONG);
+		q.addScalar("groupId", Type.LONG);
+		q.addScalar("userId", Type.LONG);
+		q.addScalar("userName", Type.STRING);
+		q.addScalar("createDate", Type.TIMESTAMP);
+		q.addScalar("modifiedDate", Type.TIMESTAMP);
+		q.addScalar("startTime", Type.TIMESTAMP);
+		q.addScalar("endTime", Type.TIMESTAMP);
+		q.addScalar("description", Type.STRING);
+		q.addScalar("taskId", Type.LONG);
 
 		QueryPos qPos = QueryPos.getInstance(q);
 
+		qPos.add(companyId);
 		qPos.add(userId);
-		qPos.add(date);
-		qPos.add(date);
+		qPos.add(date1);
+		qPos.add(date2);
 
 		List<Object[]> queriedTaskSessions =
 			(List<Object[]>) QueryUtil.list(
@@ -59,9 +68,17 @@ public class TaskSessionFinderImpl extends BasePersistenceImpl<TaskSession>
 		for (Object[] queriedTaskSession : queriedTaskSessions) {
 			TaskSessionImpl taskSession = new TaskSessionImpl();
 
-			taskSession.setStartTime((Date) queriedTaskSession[0]);
-			taskSession.setEndTime((Date) queriedTaskSession[1]);
-//			taskSession.setTaskName((String) queriedTaskSession[2]);
+			taskSession.setTaskSessionId((Long)queriedTaskSession[0]);
+			taskSession.setCompanyId((Long)queriedTaskSession[1]);
+			taskSession.setGroupId((Long)queriedTaskSession[2]);
+			taskSession.setUserId((Long)queriedTaskSession[3]);
+			taskSession.setUserName((String) queriedTaskSession[4]);
+			taskSession.setCreateDate((Date) queriedTaskSession[5]);
+			taskSession.setModifiedDate((Date) queriedTaskSession[6]);
+			taskSession.setStartTime((Date) queriedTaskSession[7]);
+			taskSession.setEndTime((Date) queriedTaskSession[8]);
+			taskSession.setDescription((String) queriedTaskSession[9]);
+			taskSession.setTaskId((Long) queriedTaskSession[10]);
 
 			taskSessions.add(taskSession);
 		}
