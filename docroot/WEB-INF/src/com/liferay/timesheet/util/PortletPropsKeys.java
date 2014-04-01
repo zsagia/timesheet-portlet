807531a010a3d4177c511f2fd53f0e201db4db67
istvansajtos <istvan.sajtos@liferay.com>
2014-04-01 15:56:10 +0200

    issue#61 Adding property handler classes

---
+++ docroot/WEB-INF/src/com/liferay/timesheet/util/PortletPropsKeys.java
@@ -0,0 +1,26 @@
+package com.liferay.timesheet.util;
+
+/**
+ * @author Istvan Sajtos
+ */
+public interface PortletPropsKeys {
+
+	public static final String RESTRICTIONS_BASETIME_END =
+		"restrictions.basetime.end";
+
+	public static final String RESTRICTIONS_BASETIME_START =
+		"restrictions.basetime.start";
+
+	public static final String RESTRICTIONS_ENDTIME_LATEST =
+		"restrictions.endtime.latest";
+
+	public static final String RESTRICTIONS_LUNCHTIME_MIN_DURATION =
+		"restrictions.lunchtime.min.duration";
+
+	public static final String RESTRICTIONS_STARTTIME_EARLIEST =
+		"restrictions.starttime.earliest";
+
+	public static final String RESTRICTIONS_WORKDURATIOIN_MAX =
+		"restrictions.workduration.max";
+
+}
