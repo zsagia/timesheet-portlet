57874fdc0b8a274ea861d5259d0601bd6293ef2c
istvansajtos <istvan.sajtos@liferay.com>
2014-04-03 17:14:05 +0200

    issue#61 Adding default tasks at deploy time

---
+++ docroot/WEB-INF/src/com/liferay/timesheet/hook/upgrade/v1_0_0/UpgradeTask.java
@@ -0,0 +1,37 @@
+package com.liferay.timesheet.hook.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.timesheet.service.TaskLocalServiceUtil;
+import com.liferay.timesheet.util.TaskConstants;
+
+/**
+ * @author Istvan Sajtos
+ */
+public class UpgradeTask extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		long companyId = PortalUtil.getDefaultCompanyId();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		Group group = GroupLocalServiceUtil.getGroup(
+			companyId, GroupConstants.GUEST);
+
+		serviceContext.setScopeGroupId(group.getGroupId());
+
+		long defaultUserId = UserLocalServiceUtil.getDefaultUserId(companyId);
+
+		TaskLocalServiceUtil.addTask(defaultUserId, "Lunch Break", 0, null,
+			serviceContext);
+		TaskLocalServiceUtil.addTask(defaultUserId, "10min Break", 0, null,
+			serviceContext);
+	}
+
+}
