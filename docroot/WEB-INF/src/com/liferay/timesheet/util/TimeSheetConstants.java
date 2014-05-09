0e4454d56e35cec54d968be589edf236e73e8403
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-05-09 08:56:18 +0200

    #issue73 Migrating properties to TimeSheetConstants

---
+++ docroot/WEB-INF/src/com/liferay/timesheet/util/TimeSheetConstants.java
@@ -0,0 +1,26 @@
+package com.liferay.timesheet.util;
+
+/**
+ * @author Istvan Sajtos
+ */
+public class TimeSheetConstants {
+
+	public static int DATE_DAY = 0;
+
+	public static int DATE_WEEK = 1;
+
+	public static int DATE_MONTH = 2;
+
+	public static final int TASK_GENERAL = 0;
+
+	public static final int TASK_LUNCH = 1;
+
+	public static final int TASK_BREAK = 2;
+
+	public static final int TASK_MANDATORY_BREAK = 3;
+
+	public static final int USER_LIST_ALL = 0;
+
+	public static final int USER_LIST_GROUPPED = 1;
+
+}
