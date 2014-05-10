f02274fc69153945d5ab1f9c84060a23dd0c3f20
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-05-10 21:05:55 +0200

    #issue75 Added StringTrimmer converter

---
+++ docroot/WEB-INF/src/com/liferay/timesheet/converter/StringTrimmer.java
@@ -0,0 +1,25 @@
+package com.liferay.timesheet.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("StringTrimmer")
+public class StringTrimmer implements Converter {
+
+	@Override
+	public Object getAsObject(
+		FacesContext context, UIComponent component, String value) {
+
+		return value != null ? value.trim() : null;
+	}
+
+	@Override
+	public String getAsString(
+		FacesContext context, UIComponent component, Object value) {
+
+		return (String)value;
+	}
+
+}
