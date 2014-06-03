92316000bef1f3d62c8578020aa02912cb9f091e
Zsigmond Rab <zsigmond.rab@liferay.com>
2014-06-03 16:47:50 +0200

    Issue#80 Create the service layer for the Day object

---
+++ docroot/WEB-INF/service/com/liferay/timesheet/model/DayConstants.java
@@ -0,0 +1,41 @@
+package com.liferay.timesheet.model;
+
+public class DayConstants {
+
+	public static final int TYPE_NATIONAL_HOLIDAY = 1;
+
+	public static final String TYPE_NATIONAL_HOLIDAY_LABEL = "national-holiday";
+
+	public static final int TYPE_EXCEPTIONAL_HOLIDAY = 2;
+
+	public static final String TYPE_EXCEPTIONAL_HOLIDAY_LABEL = "exceptional-holiday";
+
+	public static final int TYPE_EXCEPTIONAL_WORKDAY = 3;
+
+	public static final String TYPE_EXCEPTIONAL_WORKDAY_LABEL = "exceptional-workday";
+
+	public static final int TYPE_USER_HOME_OFFICE = 4;
+
+	public static final String TYPE_USER_HOME_OFFICE_LABEL = "user-home-office";
+
+	public static final int TYPE_USER_HOLIDAY = 5;
+
+	public static final String TYPE_USER_HOLIDAY_LABEL = "user-holiday";
+
+	public static final int TYPE_USER_ONSITE_ABROAD = 6;
+
+	public static final String TYPE_USER_ONSITE_ABROAD_LABEL = "user-onsite-abroad";
+
+	public static final int TYPE_USER_ONSITE_AT_HOME = 7;
+
+	public static final String TYPE_USER_ONSITE_AT_HOME_LABEL = "user-onsite-at-home";
+
+	public static final int TYPE_USER_SICKNESS_BENEFIT = 8;
+
+	public static final String TYPE_USER_SICKNESS_BENEFIT_LABEL = "user-sickness-benefit";
+
+	public static final int TYPE_USER_UNPAID_HOLIDAY = 9;
+
+	public static final String TYPE_USER_UNPAID_HOLIDAY_LABEL = "user-unpaid-holiday";
+
+}
