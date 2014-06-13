778d987c14fa87762aa1ad49221be11a4a45a57a
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-06-13 11:58:16 +0200

    #issue82 Added new methods to Day

--- docroot/WEB-INF/src/com/liferay/timesheet/model/impl/DayImpl.java
+++ docroot/WEB-INF/src/com/liferay/timesheet/model/impl/DayImpl.java
@@ -14,6 +14,9 @@
 
 package com.liferay.timesheet.model.impl;
 
+import com.liferay.timesheet.model.Day;
+import com.liferay.timesheet.model.DayConstants;
+
 /**
  * The extended model implementation for the Day service. Represents a row in the &quot;timesheet_Day&quot; database table, with each column mapped to a property of this class.
  *
@@ -30,5 +33,27 @@ public class DayImpl extends DayBaseImpl {
 	 * Never reference this class directly. All methods that expect a day model instance should use the {@link com.liferay.timesheet.model.Day} interface instead.
 	 */
 	public DayImpl() {
+	}
+
+	public String getDayType() {
+		String typeString = null;
+
+		switch (getType()) {
+			case DayConstants.TYPE_NATIONAL_HOLIDAY :
+				typeString = DayConstants.TYPE_NATIONAL_HOLIDAY_LABEL;
+				break;
+			case DayConstants.TYPE_EXCEPTIONAL_HOLIDAY :
+				typeString = DayConstants.TYPE_EXCEPTIONAL_HOLIDAY_LABEL;
+				break;
+			case DayConstants.TYPE_EXCEPTIONAL_WORKDAY :
+				typeString = DayConstants.TYPE_EXCEPTIONAL_WORKDAY_LABEL;
+		}
+
+		return typeString;
+	}
+
+	@Override
+	public int compareTo(Day day) {
+		return getDate().before(day.getDate()) ? -1 : 1;
 	}
 }
