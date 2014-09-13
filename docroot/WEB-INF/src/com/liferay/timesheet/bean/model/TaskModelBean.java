14e270af2a68dd9944f6ce905d40dffc1275a4b4
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-09-13 18:50:35 +0200

    issue#106 Remake TaskModelBean

--- docroot/WEB-INF/src/com/liferay/timesheet/bean/model/TaskModelBean.java
+++ docroot/WEB-INF/src/com/liferay/timesheet/bean/model/TaskModelBean.java
@@ -4,15 +4,27 @@ import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.timesheet.model.Project;
 import com.liferay.timesheet.model.Task;
 import com.liferay.timesheet.service.TaskLocalServiceUtil;
+import com.liferay.timesheet.service.TaskServiceUtil;
+import com.liferay.timesheet.util.ActionKeys;
+import com.liferay.timesheet.util.RoleUtil;
 import com.liferay.timesheet.util.TimeSheetConstants;
 import com.liferay.timesheet.util.TimeSheetUtil;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -25,50 +37,139 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class TaskModelBean implements Serializable {
 
+	public TaskModelBean() {
+		init();
+	}
+
 	public Task createTask(
-			long userId, long projectId, ServiceContext serviceContext)
+			long companyId, long userId, long projectId, int type,
+			ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
 		return TaskLocalServiceUtil.addTask(
-			userId, taskName, projectId, description,
-			TimeSheetConstants.TASK_GENERAL, serviceContext);
+			companyId, userId, taskName, projectId, description, type,
+			serviceContext);
+	}
+
+	public Task createTask(
+			long companyId, long userId, long projectId, int type,
+			long[] assignedUserIds, long[] assignedRoleIds,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		return TaskLocalServiceUtil.addTask(
+			companyId, userId, taskName, projectId, description, type,
+			assignedUserIds, assignedRoleIds, serviceContext);
+	}
+
+	public void deleteTask(long taskId)
+		throws PortalException, SystemException {
+
+		TaskLocalServiceUtil.deleteTask(taskId);
+	}
+
+	public List<Task> getAssignedTask()
+		throws PortalException, SystemException {
+
+		return TaskServiceUtil.getTasksByC_G_TT(
+			TimeSheetUtil.getCompanyId(), TimeSheetUtil.getScopeGroupId(),
+			TimeSheetConstants.TASK_ASSIGNED);
+	}
+
+	public void setAssignedRoles(Task task)
+		throws PortalException, SystemException {
+
+		List<Role> assignedRoles = RoleUtil.getAssignedRoles(
+			task.getCompanyId(), task, ActionKeys.VIEW);
+
+		setAssignedRoles(assignedRoles);
+	}
+
+	public void setAssignedUsers(Task task)
+		throws PortalException, SystemException {
+
+		Role role = RoleLocalServiceUtil.getRole(
+			task.getCompanyId(),
+			TimeSheetConstants.TIMESHEET_TASK_ROLE + task.getTaskId());
+
+		List<User> assignedUsers = UserLocalServiceUtil.getRoleUsers(
+			role.getRoleId());
+
+		if (!assignedUsers.isEmpty()) {
+			setAssignedUsers(ListUtil.copy(assignedUsers));
+		}
+	}
+
+	public void setRoleList() {
+		roleList = new HashMap<Long, Boolean>();
+
+		for (Role role: assignedRoles) {
+			roleList.put(role.getRoleId(), Boolean.TRUE);
+		}
+	}
+
+	public void setUserList() {
+		userList = new HashMap<Long, Boolean>();
+
+		for (User user: assignedUsers) {
+			userList.put(user.getUserId(), Boolean.TRUE);
+		}
 	}
 
 	public List<Task> getTasksByUser() {
-		long userId = TimeSheetUtil.getCurrentUserId();
+		ThemeDisplay themeDisplay = TimeSheetUtil.getThemeDisplay();
 
-		List<Task> tasksToday = null;
+		long userId = TimeSheetUtil.getCurrentUserId();
+		long groupId = themeDisplay.getScopeGroupId();
+
+		List<Task> userTasks = null;
 
 		try {
-			tasksToday = TaskLocalServiceUtil.getTasksByUserId(userId);
+			userTasks = TaskLocalServiceUtil.getTaskByU_TT(
+				userId, TimeSheetConstants.TASK_GENERAL);
 
-			Task minutes10BreakTask = TaskLocalServiceUtil.getTaskByType(
-				TimeSheetConstants.TASK_BREAK);
+			userTasks.addAll(
+				TaskServiceUtil.getTasksByC_G_U_TT(
+					TimeSheetUtil.getCompanyId(), groupId, userId,
+					TimeSheetConstants.TASK_ASSIGNED));
 
-			tasksToday.add(0, minutes10BreakTask);
+			userTasks.add(
+				0, TaskLocalServiceUtil.getTaskByType(
+					TimeSheetConstants.TASK_BREAK));
 
-			Task outOfOfficeTask = TaskLocalServiceUtil.getTaskByType(
-				TimeSheetConstants.TASK_MANDATORY_BREAK);
+			userTasks.add(
+				0, TaskLocalServiceUtil.getTaskByType(
+					TimeSheetConstants.TASK_MANDATORY_BREAK));
 
-			tasksToday.add(0, outOfOfficeTask);
-
-			Task lunchTask = TaskLocalServiceUtil.getTaskByType(
-				TimeSheetConstants.TASK_LUNCH);
-
-			tasksToday.add(0, lunchTask);
+			userTasks.add(
+				0, TaskLocalServiceUtil.getTaskByType(
+					TimeSheetConstants.TASK_LUNCH));
 		} catch (Exception e) {
 			logger.error(
 				"Getting tasks for userId: " + userId + " is failed", e);
 		}
 
-		return tasksToday;
+		return userTasks;
+	}
+
+	public void init() {
+		taskName = null;
+
+		description = null;
+
+		assignedRoles = new ArrayList<Role>();
+		assignedUsers = new ArrayList<User>();
+
+		roleList = new HashMap<Long, Boolean>();
+		userList = new HashMap<Long, Boolean>();
 	}
 
 	public Task updateTask(
-			Task task, String taskName, Project project)
+			Task task, String taskName, String description, Project project)
 		throws PortalException, SystemException {
 
 		task.setTaskName(taskName);
+		task.setDescription(description);
 
 		if (project != null) {
 			task.setProjectId(project.getProjectId());
@@ -77,25 +178,76 @@ public class TaskModelBean implements Serializable {
 		return TaskLocalServiceUtil.updateTask(task);
 	}
 
+	public Task updateTask(
+			Task task, String taskName, String description, Project project,
+			long[] assignedUserIds, long[] assignedRoleIds)
+		throws PortalException, SystemException {
+
+		task.setTaskName(taskName);
+		task.setDescription(description);
+
+		if (project != null) {
+			task.setProjectId(project.getProjectId());
+		}
+
+		return TaskLocalServiceUtil.updateTask(
+			task, assignedUserIds, assignedRoleIds);
+	}
+
+	public List<Role> getAssignedRoles() {
+		return assignedRoles;
+	}
+
+	public List<User> getAssignedUsers() {
+		return assignedUsers;
+	}
 
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public Map<Long, Boolean> getRoleList() {
+		return roleList;
 	}
 
 	public String getTaskName() {
 		return taskName;
 	}
 
+	public Map<Long, Boolean> getUserList() {
+		return userList;
+	}
+
+	public void setAssignedRoles(List<Role> assignedRoles) {
+		this.assignedRoles = assignedRoles;
+	}
+
+	public void setAssignedUsers(List<User> assignedUsers) {
+		this.assignedUsers = assignedUsers;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setRoleList(Map<Long, Boolean> roleList) {
+		this.roleList = roleList;
+	}
+
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
 
+	public void setUserList(Map<Long, Boolean> userList) {
+		this.userList = userList;
+	}
+
+	private List<Role> assignedRoles = null;
+	private List<User> assignedUsers = null;
 	private String description;
+	private Map<Long, Boolean> roleList = null;
 	private String taskName;
+	private Map<Long, Boolean> userList = null;
 
 	private static final long serialVersionUID = 4961368400508391281L;
 	private static Logger logger = LoggerFactory.getLogger(TaskModelBean.class);
